36b328f4f45005037508ad598863e83f29c38780
Çağlar Ergül <caglarergul@me.com>
2017-11-22 10:14:56 +0300

    initial state

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import registerServiceWorker from './utils/registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
