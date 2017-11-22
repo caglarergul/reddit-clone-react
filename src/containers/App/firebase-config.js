7747fdf1af92e3e1b9e4b7a55c537d4230a92219
Çağlar Ergül <caglarergul@me.com>
2017-11-22 15:00:48 +0300

    Firebase initialization

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCwmFhsMVdB6MnweWH8NbRrIJH7xBezqpE",
+    authDomain: "reddit-clone-react.firebaseapp.com",
+    databaseURL: "https://reddit-clone-react.firebaseio.com",
+    projectId: "reddit-clone-react",
+    storageBucket: "reddit-clone-react.appspot.com",
+    messagingSenderId: "8467146185"
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {browserHistory} from 'react-router';
+
 import App from './containers/App';
+import Routes from './routes';
+
 import registerServiceWorker from './utils/registerServiceWorker';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Routes history={browserHistory} />, document.getElementById('root'));
 registerServiceWorker();
